4568c47ba8af3f43fb6f8a04de7c4a9d7cef318d
okrand <orkun.krand@gmail.com>
2017-11-19 22:24:49 -0500

    Finally working on it

---
+++ app.js
@@ -0,0 +1,9 @@
+var app = angular.module('orkunModule', []);
+app.controller("orkunCtrl", function ($scope) {
+    $scope.firstName = "Orkun";
+    $scope.lastName = "Krand";
+    $scope.email = "mailto:orkun.krand@gmail.com";
+    $scope.twitter = "https://twitter.com/Krando67";
+    $scope.linkedin = "https://www.linkedin.com/in/orkun-krand/";
+    $scope.github = "https://github.com/okrand";
+});
