24b99faa7bcd65e2d1302cbb1c545e1db407cfa2
okrand <orkun.krand@gmail.com>
2018-02-21 14:58:13 -0500

    NG-Cloak

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.controller("orkunCtrl", function ($scope) {
     $scope.twitter = "https://twitter.com/Krando67";
     $scope.linkedin = "https://www.linkedin.com/in/orkun-krand/";
     $scope.github = "https://github.com/okrand";
+    $scope.highside = "https://github.com/okrand/Web-Programming";
+    $scope.triathlon = "https://github.com/okrand/Triathlon_Trainer";
     $scope.youtube = "https://www.youtube.com/okmotorcu";
     $scope.resume = "./Resume_Orkun_Krand.pdf";
 });
