1a985a36a1491a995df719cca7ae225905fdd747
okrand <orkun.krand@gmail.com>
2018-03-28 07:45:37 -0400

    Updated Resume and link

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.controller("orkunCtrl", function ($scope) {
     $scope.github = "https://github.com/okrand";
     $scope.highside = "https://github.com/okrand/Web-Programming";
     $scope.triathlon = "https://github.com/okrand/Triathlon_Trainer";
+    $scope.herdr = "https://github.com/okrand/Herdr";
     $scope.youtube = "https://www.youtube.com/okmotorcu";
     $scope.resume = "./Resume_Orkun_Krand.pdf";
 });
